7a9b3aaf55b5e3773f8004a35d72c06e82dc8eea
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-01 02:23:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,9 +112,9 @@ def play(board)
   until over?(board) do
     turn(board)
   end
-  if won?(board) 
+  if won?(board)
     puts "Congratulations #{winner(board)}!"
-  elsif draw?(board) 
+  elsif draw?(board)
     puts "Cat's Game!"
   end
 end
